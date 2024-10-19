e6ec86a7367efb54cc28c5fe282df467a4ed5f16
Khittimasug <khittimasug.j@kkumail.com>
2024-10-19 22:00:34 +0700

    PlssHelpme

--- my_sql/my_sql/settings.py
+++ my_sql/my_sql/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Test'
+    'Test',
+    'widget_tweaks',
 ]
+
+'''AUTHENTICATION_BACKENDS=[
+    'django.countrib.auth.backedns.ModelBackend'
+]'''
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATICFILES_DIRS = [BASE_DIR/"static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
